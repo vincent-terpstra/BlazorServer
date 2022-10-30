f9086e5e07ae92d874ea7b1fbff1665e34a1e3cd
Vincent Terpstra <vdterpst@gmail.com>
2022-10-30 17:31:45 -0400

    Added AddUserPage.razor form, and submit user to add to database and user validation

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -1,12 +1,25 @@
 ﻿// ReSharper disable InconsistentNaming
+
+using System.ComponentModel.DataAnnotations;
+
 namespace DataAccess.Models;
 
 public class UserModel
 {
     public int id { get; set; }
 
+    [Required]
+    [MaxLength(15, ErrorMessage = "First Name is too long")]
+    [MinLength(5, ErrorMessage = "First Name is too short")]
     public string firstname { get; set; }
 
+    [Required]
+    [MaxLength(15, ErrorMessage = "Last Name is too long")]
+    [MinLength(5, ErrorMessage = "Last Name is too short")]
     public string lastname { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string email { get; set; }
 
 }
