797294799f3fcffe24e4bad5fb2b36d1b48d9238
Vincent Terpstra <vdterpst@gmail.com>
2022-10-30 16:59:36 -0400

    removed initial counter and weatherforecast pages and data

--- BlazorServerDemo/Program.cs
+++ BlazorServerDemo/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using BlazorServerDemo.Data;
 using DataAccess.DbAccess;
 using DataAccess.Services;
 
@@ -10,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
 builder.Services.AddSingleton<IUserService, UserService>();
 
