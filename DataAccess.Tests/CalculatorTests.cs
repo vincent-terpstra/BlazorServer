0dae40936a875935c4464a4ac419ee3bec98eabf
Vincent Terpstra <vdterpst@gmail.com>
2022-11-03 10:14:57 -0400

    Added testing with double.NaN and double.PositiveInfinity and double.NegativeInfinity

--- DataAccess.Tests/CalculatorTests.cs
+++ DataAccess.Tests/CalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace DataAccess.Tests;
@@ -24,6 +25,8 @@ public class CalculatorTests
     [Theory]
     [InlineData(5, 5, 10)]
     [InlineData(2, 3, 5)]
+    [InlineData(double.MaxValue, 5, double.MaxValue)]
+    [InlineData(double.NaN, double.NaN, double.NaN)]
     public void Add_SimpleValues_Should_Calculate(double x, double y, double expected)
     {
         //Arrange
@@ -37,6 +40,8 @@ public class CalculatorTests
     [Theory]
     [InlineData(5, 5, 25)]
     [InlineData(2, -3, -6)]
+    [InlineData(double.PositiveInfinity, -1, double.NegativeInfinity)]
+    [InlineData(double.NaN, double.NaN, double.NaN)]
     public void Multiply_SimpleValues_Should_Calculate(double x, double y, double expected)
     {
         //Arrange
@@ -50,6 +55,8 @@ public class CalculatorTests
     [Theory]
     [InlineData(5, -5, -1)]
     [InlineData(10, 20, .5)]
+    [InlineData(5, double.NegativeInfinity, 0)]
+    [InlineData(double.NaN, double.NaN, double.NaN)]
     public void Divide_SimpleValues_Should_Calculate(double x, double y, double expected)
     {
         //Arrange
@@ -71,6 +78,7 @@ public class CalculatorTests
         
         //Assert
         //note normally dividing by zero returns double.PositiveInfinity
+        //throwing is specific to my Calculator.Divide
         Assert.Throws<DivideByZeroException>(() =>Calculator.Divide(x, y));
     }
 }
