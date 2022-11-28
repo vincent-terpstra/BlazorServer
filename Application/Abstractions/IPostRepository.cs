4d78584bf61fa026276786558154afb6d3a413fd
Vincent Terpstra <vdterpst@gmail.com>
2022-11-28 17:21:57 -0500

    Data access for Posts using EntityFramework

---
+++ Application/Abstractions/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models;
+
+namespace Application.Abstractions;
+
+public interface IPostRepository
+{
+    Task<ICollection<Post>> GetAllPosts();
+    Task<Post?> GetPostById(int id);
+    Task<Post> CreatePost(Post create);
+    Task<Post> UpdatePost(string updatedContent, int postId);
+    Task DeletePost(int postId);
+}
