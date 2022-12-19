5507a1c3adb99dbbdba1ab7ad8f90e9dabb37e1b
Vincent Terpstra <vdterpst@gmail.com>
2022-12-18 21:02:01 -0500

    Refactor into class
    moving into public class for testing purposes

--- MinimalAPIDemo/Program.cs
+++ MinimalAPIDemo/Program.cs
@@ -1,32 +1,40 @@
 using Application.Abstractions;
 using DataAccess;
-using DataAccess.Repositories;
-using MinimalAPIDemo;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.RegisterServices();
-var app = builder.Build();
+namespace MinimalAPIDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        var builder = WebApplication.CreateBuilder(args);
+        builder.RegisterServices();
+
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-    var scope = app.Services.CreateScope();
-    var users = scope.ServiceProvider.GetRequiredService<IUserService>();
-    users.PopulateDbUsers();
+            var scope = app.Services.CreateScope();
+            var users = scope.ServiceProvider.GetRequiredService<IUserService>();
+            users.PopulateDbUsers();
 
-    var posts = scope.ServiceProvider.GetRequiredService<IPostRepository>();
-    posts.PopulateDbPosts();
+            var posts = scope.ServiceProvider.GetRequiredService<IPostRepository>();
+            posts.PopulateDbPosts();
 
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.RegisterExceptionHandling();
+        app.RegisterEndpoints();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.RegisterExceptionHandling();
-app.RegisterEndpoints();
-
-app.Run();
