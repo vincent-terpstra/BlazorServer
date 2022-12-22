80127efd5668069a9d2db1d81eee1632d30b75eb
Vincent Terpstra <vdterpst@gmail.com>
2022-12-21 22:39:48 -0500

    Adding Cascading values to only save when needed

---
+++ BlazorServerDemo/Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿using BlazorServerDemo.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerDemo.Pages;
+
+public partial class Index
+{
+    [CascadingParameter]
+    private MyPageLock? UnsavedChanges { get; set; }
+
+    private void EnterEditMode()
+    {
+        UnsavedChanges?.SetUnSavedChanges(true);
+    }
+}
--- BlazorServerDemo/Shared/MyPageLock.razor.cs
+++ BlazorServerDemo/Shared/MyPageLock.razor.cs
@@ -6,7 +6,15 @@ namespace BlazorServerDemo.Shared;
 public partial class MyPageLock
 {
     [Inject] private NavigationManager NavigationManager { get; set; } = null!;
-
+    
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    
+    public void SetUnSavedChanges(bool b)
+    {
+        UnsavedChanges = b;
+    }
+    
     public MyPageLock()
     {
         _myLocationChangingHandler = context =>
@@ -34,18 +42,17 @@ public partial class MyPageLock
     }
 
     private readonly Func<LocationChangingContext, ValueTask> _myLocationChangingHandler;
-
+    
     public bool NavigationActive { get; set; }
 
-    [Parameter] public bool UnsavedChanges { get; set; } = true;
+    [Parameter] public bool UnsavedChanges { get; set; }
 
     private void ConfirmNavigation()
     {
         UnsavedChanges = false;
         NavigationActive = false;
-        
         NavigationManager.NavigateTo(TargetLocation);
-        //HACK - reenable unsaved changes
-        UnsavedChanges = true;
     }
+
+    
 }
