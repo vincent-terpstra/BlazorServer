beff4da459a98dfdf998f8ad5b0fd6bb6bdf35eb
Vincent Terpstra <vdterpst@gmail.com>
2023-01-04 18:50:37 -0500

    Refactoring commit

---
+++ Application/Posts/CommandHandlers/DeletePostHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Abstractions;
+using Application.Posts.Commands;
+using MediatR;
+
+namespace Application.Posts.CommandHandlers;
+
+public class DeletePostHandler :IRequestHandler<DeletePost>
+{
+    private readonly IPostRepository _repository;
+
+    public DeletePostHandler(IPostRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(DeletePost request, CancellationToken cancellationToken)
+    {
+        await _repository.DeletePost(request.Id);
+        return Unit.Value;
+    }
+}
