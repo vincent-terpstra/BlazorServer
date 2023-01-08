4d791454c0492f117343101e0f2ff3d5313641da
Vincent Terpstra <vdterpst@gmail.com>
2023-01-07 19:35:51 -0500

    NDC Blazor Components

---
+++ BlazorServerDemo/Pages/MoviesTest/DataTableHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace BlazorServerDemo.Pages.MoviesTest;
+
+internal static class DataTableHelpers
+{
+    public static IEnumerable<PropertyInfo> GetItemsPropertyInfo<TItem>() 
+        => typeof(TItem)
+        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .Where(x => x.CanRead);
+}
---
+++ BlazorServerDemo/Pages/MoviesTest/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorServerDemo.Pages.MoviesTest;
+
+public class Movie
+{
+    public int  Id { get; set; }
+    public string Title { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+}
