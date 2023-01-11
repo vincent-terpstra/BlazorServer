5e4d9b1cf2e7c72c379ed647877f4830a9c95598
Vincent Terpstra <vdterpst@gmail.com>
2023-01-10 22:04:45 -0500

    Testing EditContext isModified function and pagelock

---
+++ BlazorServerDemo/Pages/Forms/SomeModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace BlazorServerDemo.Pages.Forms;
+
+class SomeModel
+{
+    public bool BooleanProp { get; set; }
+    public DateTime? DateTimeProp { get; set; }
+    public int IntProp { get; set; }
+    public decimal DecimalProp { get; set; }
+    public string? StringProp { get; set; }
+    public string? SomeMultilineProp  { get; set; }
+    public SomeStateEnum SomeSelectProperty { get; set; } = SomeStateEnum.Pending;
+    public string? SelectedString { get; set; }
+    public List<string> ListProp { get; set; } = new(){"Hello", "World"};
+    public Color SelectedColor { get; set; }
+}
+
+public enum SomeStateEnum
+{
+    Pending,
+    Active,
+    Suspended
+}
