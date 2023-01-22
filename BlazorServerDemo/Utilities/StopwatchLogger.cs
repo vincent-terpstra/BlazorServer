2e09694cd27a91e97f542d8480242eaa12164c0b
Vincent Terpstra <vdterpst@gmail.com>
2023-01-21 19:37:52 -0500

    Added disposable stopwatch logger

---
+++ BlazorServerDemo/Utilities/StopwatchLogger.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace BlazorServerDemo.Utilities;
+
+public class StopwatchLogger : IDisposable
+{
+    private readonly Stopwatch _stopwatch;
+    private readonly LogLevel _loglevel;
+    private readonly ILogger _logger;
+    private readonly string _message;
+    private readonly object?[] _args;
+
+    public StopwatchLogger(ILogger logger, LogLevel loglevel, string message)
+    {
+        _logger = logger;
+        _message = message;
+        _loglevel = loglevel;
+        _stopwatch = Stopwatch.StartNew();
+    }
+    
+    public StopwatchLogger(ILogger logger, LogLevel loglevel, string message, object?[] args)
+    {
+        _logger = logger;
+        _message = message;
+        _loglevel = loglevel;
+        _args = args;
+        _stopwatch = Stopwatch.StartNew();
+    }
+
+    public void Dispose()
+    {
+        _stopwatch.Stop();
+        _logger.Log(_loglevel, $"{_message} took {_stopwatch.ElapsedMilliseconds}ms", _args);
+    }
+}
---
+++ BlazorServerDemo/Utilities/UtilityExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BlazorServerDemo.Utilities;
+
+public static class UtilityExtensionMethods
+{
+    public static StopwatchLogger StartStopwatch<T>(this ILogger<T> logger, string message, params object?[] args)
+    {
+        return new StopwatchLogger(logger, LogLevel.Information, message, args );
+    }
+
+    public static StopwatchLogger BeginStopwatch<T>(this ILogger<T> logger, [CallerMemberName] string memberName = "")
+    {
+        return new StopwatchLogger(logger, LogLevel.Information, memberName);
+    }
+}
